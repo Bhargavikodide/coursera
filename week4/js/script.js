a844809bde8c2274d09d12fc625abd000b533f35
Bhargavikodide <67697798+Bhargavikodide@users.noreply.github.com>
2020-07-09 20:34:08 +0530

    Update script.js

--- week4/js/script.js
+++ week4/js/script.js
@@ -1,4 +1,4 @@
-var a=['John','jyothi','Ravi','sneha'];
+var a=['yaakov','john','Jen','jason','paul','Frank','Larry','Paula','Laura','Jim'];
 for(name=0 ; name<a.length ; name++) {
 if(a[name].charAt(0)=='j' || a[name].charAt(0)=='J') {
 console.log("Goodbye" + a[name]);
