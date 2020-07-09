1b04f125b486857d0783af4d23ff17847659edee
Bhargavikodide <67697798+Bhargavikodide@users.noreply.github.com>
2020-07-09 20:20:40 +0530

    Create script.js

---
+++ week4/js/script.js
@@ -0,0 +1,9 @@
+var a=['John','jyothi','Ravi','sneha'];
+for(name=0 ; name<a.length ; name++) {
+if(a[name].charAt(0)=='j' || a[name].charAt(0)=='J') {
+console.log("Goodbye" + a[name]);
+}
+else {
+ console.log("Hello" + a[name]);
+}
+}
